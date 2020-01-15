c187643ecc135c2fe41d57bf3fcc819787617f06
d-yosh <daicedaice87@gmail.com>
2020-01-15 17:00:35 +0900

    isSortedを実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,16 @@
+import scala.annotation.tailrec
+
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    @tailrec
+    def isSortedRec(i: Int): Boolean = {
+      if (i == sortedSeq.length - 1) true
+      else if (!ordered(sortedSeq(i), sortedSeq(i + 1))) false
+      else isSortedRec(i + 1)
+    }
+
+    if (sortedSeq.isEmpty) true else isSortedRec(0)
+  }
 
 }
